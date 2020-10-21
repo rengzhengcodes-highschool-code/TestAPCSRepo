4b57d817f90ba6b5478e340ff54be1542f18bed7
rengzhengcodes <rzhengcontacts@gmail.com>
2020-10-21 19:26:09 -0400

    Tester file for Demo.java

---
+++ tester.java
@@ -0,0 +1,22 @@
+public class tester {
+  public static void main(String[] args) {
+    int[][] arr = {
+      {},
+      {1},
+      {1,2},
+      {1,2,3},
+      {1,2,3,4},
+      {1,2,3,4,5}
+    };
+    int[][] arr1 = {
+      {}
+    };
+    int[][] arr2 = {{1, 2, 3}, {}, {8, 2, 3, 1}, {1}};
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(Demo.arrToString(arr[i]));
+    }
+    System.out.println(Demo.arrayDeepToString(arr));
+    System.out.println(Demo.arrayDeepToString(arr1));
+    System.out.println(Demo.arrayDeepToString(arr2));
+  }
+}
